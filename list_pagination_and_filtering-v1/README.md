ca57afea2c0d6bba2b68a86270b50951d8df6c17
katherine <hurstkatherine@hotmail.co.uk>
2020-05-17 20:05:46 +0100

    Added pagination and made a start on search bar functionality

---
+++ list_pagination_and_filtering-v1/README.md
@@ -0,0 +1,9 @@
+THis project requires the pagination of a list of fictional students, as well as implementing a search function to narrow down the search.
+
+This project will involve "progressive enhancement", which should mean the original page will function acceptably without javascript enabled, but is enhanced by the inclusion of javascript.
+
+I've attempted the "Exceeds Expectations" grade within this project.
+
+Extra credit features:
+
+- a seach bar function
--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -2,9 +2,8 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 /*** 
    Add your global variables that store the DOM elements you will 
@@ -17,8 +16,8 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
-
-
+let listItems = document.getElementsByClassName("student-item");
+let itemsPerPage = 10;
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -35,16 +34,90 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+function showPage(list, page) {
+  const startIndex = page * itemsPerPage - itemsPerPage;
+  const endIndex = page * itemsPerPage;
 
-
+  for (let i = 0; i < list.length; i++)
+    if (i >= startIndex && i < endIndex) {
+      list[i].style.display = "block";
+    } else {
+      list[i].style.display = "none";
+    }
+}
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
+function appendLinks(list) {
+  let containerDiv = document.createElement("div");
+  containerDiv.className = "pagination";
+  let pageDiv = document.querySelector(".page");
+  pageDiv.appendChild(containerDiv);
 
+  const numberOfPages = Math.ceil(list.length / itemsPerPage);
 
+  let ul = document.createElement("ul");
+  containerDiv.appendChild(ul);
 
+  for (let i = 0; i < numberOfPages; i++) {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    let a = document.createElement("a");
+    a.href = "#";
+    a.textContent = i + 1;
+    a.addEventListener("click", clickHandler);
+    if (i === 0) {
+      a.className = "active";
+    }
+    li.appendChild(a);
+  }
+}
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+function clickHandler(e) {
+  let aArray = document.getElementsByTagName("a");
+  for (let child in aArray) {
+    aArray[child].className = "inactive";
+  }
+  e.target.className = "active";
+  showPage(listItems, parseInt(e.target.textContent));
+}
+
+function searchBar() {
+  let header = document.querySelector(".page-header");
+  let searchDiv = document.createElement("div");
+  searchDiv.className = "student-search";
+  let searchInput = document.createElement("input");
+  searchInput.placeholder = "Search for students";
+  let searchButton = document.createElement("button");
+  searchButton.innerHTML = "Search";
+
+  header.appendChild(searchDiv);
+  searchDiv.appendChild(searchInput);
+  searchDiv.appendChild(searchButton);
+
+  searchInput.addEventListener("keyup", searchFunc);
+}
+
+function searchFunc(e) {
+  let nameArray = [];
+  let nameElements = document.getElementsByTagName("h3");
+  for (i = 0; i < nameElements.length; i++) {
+    nameArray[i] = nameElements[i].innerHTML;
+  }
+  let input = e.target.value;
+
+  for (let i = 0; i < listItems.length; i++)
+    if (input === nameArray[i]) {
+      listItems[i].style.display = "block";
+    } else {
+      listItems[i].style.display = "none";
+    }
+}
+
+searchBar();
+
+showPage(listItems, 1);
+appendLinks(listItems);
